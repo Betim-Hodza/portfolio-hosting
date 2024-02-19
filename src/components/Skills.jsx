4b7930c63f26cf12ae0fc772906f44c93bbec050
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-02-19 06:04:04 +0000

    Its fixed noww

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+{/*
 import { FaCode } from "react-icons/fa"
 
 const [activeSection, setActiveSection] = useState("skills");
@@ -6,17 +7,19 @@ const data = {
   languages: ["C/C++", "Java", "Python", "React"],
   tools: ["gcc/g++", "gdb", "git/github", "bash"],
   os: ["Windows", "macOS", "Linux"],
+  <h1>Skills</h1>
+   
+    <button></button>
+    <button></button>
+    <button></button>
 };
+
+ */}
 
 
 const Skills = () => {
   return (
     <div>
-      <h1>Skills</h1>
-       
-        <button></button>
-        <button></button>
-        <button></button>
     </div>
   )
 }
