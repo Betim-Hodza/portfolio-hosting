3ec626601ac486088d8ecb1ce591e5476f93dff3
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-02-21 15:19:44 +0000

    Got workitem to now list each detail

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,9 +5,24 @@ const data = [
     {
         year: 2024,
         title: 'CyberSecurity Club UTA | MemberShip Officer',
-        duration: '2 years',
-        details:'blah blah blah blah blah blah blah blah blah '
+        duration: '2023-Prsent',
+        details:[
+            '- Contributed to the NCL Red Cats achievement of a high national ranking in the NCL Fall game 2023 and NCL Spring game 2024.',
+            '- Planned and presented workshops/activities, including CTFS and Network Security, boosting problem-solving skills through real-world challenges.',
+          ],
     },
+    {
+        year: 2023,
+        title: 'UTA Red Cats | NCL Team Games',
+        duration: 'Fall 2023',
+        details:  [
+            '- One of the highest ranking teams in NCL',
+            '- Top 3%, 98th National Percentile',
+            '- Placed 111 out of 4672 National Rank',
+            '- 1455/3000 Points Total',
+          ],
+    },
+    /*
     {
         year: 2024,
         title: 'MemberShip Officer',
@@ -20,12 +35,8 @@ const data = [
         duration: '2 years',
         details: 'blah blah blah blah blah blah blah blah blah '
     },
-    {
-        year: 2024,
-        title: 'MemberShip Officer',
-        duration: '2 years',
-        details: 'blah blah blah blah blah blah blah blah blah '
-    },
+    
+    */
 ]
 
 const Work = () => {
@@ -38,7 +49,7 @@ const Work = () => {
                 year={item.year} 
                 title={item.title} 
                 duration={item.duration} 
-                details={item.details} 
+                details={item.details}
                 />
             ))}
         </div>
