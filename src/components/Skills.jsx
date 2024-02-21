b3c2a104fb4c7f189d1956c528ab518d2af3c5f9
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-02-21 15:50:08 +0000

    Skills not completed but getting better

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,27 +1,61 @@
-import React from 'react'
-{/*
-import { FaCode } from "react-icons/fa"
+import React, { useState } from 'react';
+import { FaCode, FaTools, FaLaptopCode } from 'react-icons/fa';
 
-const [activeSection, setActiveSection] = useState("skills");
 const data = {
-  languages: ["C/C++", "Java", "Python", "React"],
-  tools: ["gcc/g++", "gdb", "git/github", "bash"],
-  os: ["Windows", "macOS", "Linux"],
-  <h1>Skills</h1>
-   
-    <button></button>
-    <button></button>
-    <button></button>
+  languages: {
+    title: 'Languages:',
+    skills: ['C/C++', 'Java', 'Python', 'React'],
+    icon: <FaCode />,
+  },
+  tools: {
+    title: 'Tools:',
+    skills: ['gcc/g++', 'gdb', 'git/github', 'bash'],
+    icon: <FaTools />,
+  },
+  os: {
+    title: 'Operating Systems:',
+    skills: ['Windows', 'Linux'],
+    icon: <FaLaptopCode />,
+  },
 };
 
- */}
-
-
 const Skills = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+  const [activeSection, setActiveSection] = useState(null); // Initially, no section is active
 
-export default Skills
+  const handleClick = (sectionName) => {
+    setActiveSection(sectionName === activeSection ? null : sectionName);
+  };
+
+  return (
+    <div className="skills-section">
+      <h1 className="text-4xl font-bold text-center text-[#53306b]">Skills</h1>
+      <div className="skill-buttons text-center">
+        {Object.keys(data).map((sectionName) => (
+          <button
+            key={sectionName}
+            onClick={() => handleClick(sectionName)}
+            className={`skill-button rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300 ${
+              activeSection === sectionName ? 'active' : ''
+            }`}
+          >
+            {data[sectionName].icon}
+          </button>
+        ))}
+      </div>
+      <div className="skill-details max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+        {activeSection && (
+          <div className="skill-group active">
+            <h2 className='text-lg font-semibold text-[#59227e]'>{data[activeSection].title}</h2>
+            <ul className='details ul mb-2 text-base font-normal text-stone500'>
+              {data[activeSection].skills.map((skill) => (
+                <li key={skill}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
