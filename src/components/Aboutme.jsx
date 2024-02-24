f8f3637ae5fa483628f16e8ce19ec8381522f3ce
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-02-24 20:06:25 +0000

    Fix: almost made a huge mistake but its fixed!

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aboutme = () => {
+    return (
+      <div></div>
+    )
+  }
+ 
+export default Aboutme;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
-import Project1 from '../assets/react.svg' //(doesnt exist yet but will whenever i have images for now svg filler)
-import Project2 from '../assets/react.svg' //(doesnt exist yet but will whenever i have images for now svg filler)
-import Project3 from '../assets/react.svg' //(doesnt exist yet but will whenever i have images for now svg filler)
-import Project4 from '../assets/react.svg' //(doesnt exist yet but will whenever i have images for now svg filler)
+import Project1 from '../assets/ConnectForJava.png' //(doesnt exist yet but will whenever i have images for now svg filler)
+import Project2 from '../assets/Portfolio.png' //(doesnt exist yet but will whenever i have images for now svg filler)
+import Project3 from '../assets/MTMS.png' //(doesnt exist yet but will whenever i have images for now svg filler)
 
 const Projects = () => {
     return(
         <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
             <h1 className='text-4xl font-bold text-center text-[#53306b]'>Projects</h1>
-            <p className='text-center py-8'>Filler Filler Filler Filler Blah BlahBlahBlah
-            BlahBlahBlahBlah BlahBlahBlah Blah Blah Blah
-            </p>
-            <div className='grid sm:grid-cols-2 gap-12'>
-                <ProjectItem img={Project1} title='Project #1' />
-                <ProjectItem img={Project2} title='Project #2' />
-                <ProjectItem img={Project3} title='Project #3' />
-                <ProjectItem img={Project4} title='Project #4' />
+            <p className='text-center py-8 text-xl'>Take a look at some of my project I've worked on!</p>
+            <div className='grid sm:grid-cols-2 gap-12 text-black'>
+                <ProjectItem img={Project1} title='Connect For Java' desc='The Super fun board game!' link='https://github.com/Betim-Hodza/ConnectForJava' />
+                <ProjectItem img={Project2} title='Portfolio Hosting ' desc='I feel like im having deja vu' link='https://github.com/Betim-Hodza/portfolio-hosting' />
+                <ProjectItem img={Project3} title='Movie Theatre Management System' desc='Ill be seeing you, mr Wick.' link='https://github.com/Betim-Hodza/Movie-Theater-Management-System' />
             </div>
         </div>
     )
