aedb964b6612d5ecca7fc49fe37c76506913fd00
betim-hodza <bhodza02@gmail.com>
2024-03-04 21:51:29 -0600

    Feat: added a new project Update: new face for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
 import Project1 from '../assets/Connect For Java.gif' //(doesnt exist yet but will whenever i have images for now svg filler)
-import Project2 from '../assets/Portfolio.png' //(doesnt exist yet but will whenever i have images for now svg filler)
+import Project2 from '../assets/portfolio-hosting.gif' //(doesnt exist yet but will whenever i have images for now svg filler)
 import Project3 from '../assets/Movie Theater Management System.gif' //(doesnt exist yet but will whenever i have images for now svg filler)
+import Project4 from '../assets/CSEC-CTF.gif' //(doesnt exist yet but will whenever i have images for now svg filler)
 
 const Projects = () => {
     return(
@@ -13,6 +14,7 @@ const Projects = () => {
                 <ProjectItem img={Project1} title='Connect For Java' desc='The Super fun board game!' link='https://github.com/Betim-Hodza/ConnectForJava' />
                 <ProjectItem img={Project2} title='Portfolio Hosting ' desc='I feel like im having deja vu' link='https://github.com/Betim-Hodza/portfolio-hosting' />
                 <ProjectItem img={Project3} title='Movie Theatre Management System' desc='Ill be seeing you, mr Wick.' link='https://github.com/Betim-Hodza/Movie-Theater-Management-System' />
+                <ProjectItem img={Project4} title='CSEC-CTF-Notes' desc='Notes repository website!' link='https://github.com/Betim-Hodza/CSEC-CTF-REP' />
             </div>
         </div>
     )
